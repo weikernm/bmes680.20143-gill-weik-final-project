3e63226021face825f1f2e1886ebef2eaed2a594
weikernm <weikernm@gmail.com>
2015-05-20 09:37:28 -0400

    Update BMESproject.m

--- BMESproject.m
+++ BMESproject.m
@@ -1,12 +1,21 @@
+
+
+if ~exist('GSE44772.txt','file')
 gsedata=getgeodata('GSE44772', 'ToFile','GSE44772.txt');
+else
+    gsedata=geoseriesread('GSE44772.txt');
+end
+if ~exist('genes.txt','file')
 genes=getgeodata(gsedata.Header.Series.platform_id,'ToFile','genes.txt');
+else
+    genes=geosoftread('genes.txt');
+end
 
 %% filter data
-expvalues=gsdata.Data;
+expvalues=gsedata.Data;
 gene=genes.Data;
-[mask, expvalues, genesfilt] = genelowvalfilter(expvalues,gene,...
-                                              'absval',log2(4));
-numel(genesfilt)
+[mask,Fdata] = genelowvalfilter(expvalues,'absval',log2(2));
+[Fmask,Filtdata]=geneentropyfilter(Fdata,'Percentile',30);  
 
 %% heirarchical clustering
 % use clustergram
