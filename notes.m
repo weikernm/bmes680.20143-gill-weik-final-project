1afcae070993fc749ebd18150ef05ce9c04e648b
Jason Daniel Augustine Gilliland <jdagilliland@gmail.com>
2015-05-26 14:52:30 -0400

    Flesh out notes about data structure

---
+++ notes.m
@@ -0,0 +1,6 @@
+gsedata.Header.Samples.source_name_ch1 % is the brain region
+gsedata.Header.Samples.characteristics_ch2 % is the disease state, and
+% gender, and age
+gsedata.Header.Samples.title % is a numerical sample id attached to the
+% tissue region.
+genes.Data(:,3) % gives us basically gene symbols
