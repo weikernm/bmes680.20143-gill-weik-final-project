e03daaf4ba5dbb4b272a8a148bc813beef3bfd0f
Jason Daniel Augustine Gilliland <jdagilliland@gmail.com>
2015-05-26 14:49:40 -0400

    Fix data caching to prevent costly downloading

--- project.m
+++ project.m
@@ -1,12 +1,27 @@
 if ~exist('GSE44772.txt','file')
+    % Only download data from net if file not already in directory.
+    disp('Retrieving GSE data...')
     gsedata=getgeodata('GSE44772', 'ToFile','GSE44772.txt');
-else
+    disp('done!')
+elseif ~exist('gsedata', 'var')
+    % Only load from file if not already in workspace.
+    disp('Loading GSE data...')
     gsedata=geoseriesread('GSE44772.txt');
-end
-if ~exist('genes.txt','file')
-    genes=getgeodata(gsedata.Header.Series.platform_id,'ToFile','genes.txt');
+    disp('done!')
 else
+end
+
+if ~exist('genes.txt','file')
+    % Only download data from net if file not already in directory.
+    disp('Retrieving platform data...')
+    genes=getgeodata(gsedata.Header.Series.platform_id,'ToFile','genes.txt');
+    disp('done!')
+elseif ~exist('genes', 'var')
+    % Only load from file if not already in workspace.
+    disp('Loading platform data...')
     genes=geosoftread('genes.txt');
+    disp('done!')
+else
 end
 
 %% filter data
